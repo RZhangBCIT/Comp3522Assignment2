7fdd647eff8367afdc43f9c25ea3f85b2a7c9351
Ray Zhang <rzhang104@my.bcit.ca>
2022-11-23 19:21:13 -0800

    Created Tour class

---
+++ Tour.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rzhan on 2022-11-23.
+//
+
+#include "Tour.hpp"
---
+++ Tour.hpp
@@ -0,0 +1,14 @@
+//
+// Created by rzhan on 2022-11-23.
+//
+
+#ifndef LAB1TEMPLATE_TOUR_HPP
+#define LAB1TEMPLATE_TOUR_HPP
+
+
+class Tour {
+
+};
+
+
+#endif //LAB1TEMPLATE_TOUR_HPP
