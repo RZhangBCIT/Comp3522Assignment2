3bc9a496df48d066cd63599aaf798e4ce6dc45a3
Ray Zhang <rzhang104@my.bcit.ca>
2022-11-24 01:40:36 -0800

    Created TourManager class

---
+++ TourManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rzhan on 2022-11-24.
+//
+
+#include "TourManager.hpp"
---
+++ TourManager.hpp
@@ -0,0 +1,14 @@
+//
+// Created by rzhan on 2022-11-24.
+//
+
+#ifndef LAB1TEMPLATE_TOURMANAGER_HPP
+#define LAB1TEMPLATE_TOURMANAGER_HPP
+
+
+class TourManager {
+
+};
+
+
+#endif //LAB1TEMPLATE_TOURMANAGER_HPP
