037ae0b1a8cffc691d0773a0cbf764e4352114aa
Ray Zhang <rzhang104@my.bcit.ca>
2022-11-24 01:13:42 -0800

    Added functions to Tour class

--- Tour.cpp
+++ Tour.cpp
@@ -3,3 +3,95 @@
 //
 
 #include "Tour.hpp"
+#include <random>
+#include <algorithm>
+
+using namespace std;
+void Tour::randomizeOrder() {
+    random_device random;
+    mt19937 generator(random());
+    shuffle(cities.begin(), cities.end(), generator);
+}
+
+Tour::Tour(const Tour &other) {
+    cities = other.cities;
+    fitness = other.fitness;
+}
+
+void Tour::checkFitness() {
+    fitness = 1/getDistance();
+}
+
+double Tour::getDistance() {
+    double distance = 0;
+    for (int i = 0; i < (int) cities.size() - 1; i++) {
+        distance += cities[i].getDistance(cities[i+1]);
+    }
+    distance += cities[0].getDistance(cities[cities.size()]);
+}
+
+void Tour::mutate() {
+    random_device random;
+    mt19937 generator(random());
+    uniform_real_distribution<> distribution{};
+    double coefficient_of_mutation = distribution(generator);
+    for (int i = 0; i < (int) cities.size(); i++) {
+        if (MUTATION <= coefficient_of_mutation) {
+            continue;
+        }
+        if (i == 0) {
+            swap(cities[0], cities[i]);
+            continue;
+        }
+        if (i == (int) cities.size()) {
+            swap(cities[cities.size() - 1], cities[i]);
+            continue;
+        }
+        double beforeOrAfter =distribution(generator);
+        if (beforeOrAfter < 0.5) {
+            swap(cities[i - 1], cities[i]);
+        } else {
+            swap(cities[i+1], cities[i]);
+        }
+    }
+    checkFitness();
+}
+
+void Tour::addCity(City &city) {
+    cities.push_back(city);
+}
+
+double Tour::getFitness() {
+    return fitness;
+}
+
+vector<City> &Tour::getCities() {
+    return cities;
+}
+
+bool Tour::hasCity(City &city) {
+    for (City &c : cities) {
+        if (c == city) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Tour &Tour::operator=(Tour other) {
+    swapTour(*this, other);
+    return *this;
+}
+
+void swapTour(Tour &first, Tour &second) {
+    swap(first.cities, second.cities);
+    swap(first.fitness, second.fitness);
+}
+
+ostream &operator << (ostream &os, Tour &tour) {
+    os << "Distance: " << tour.getDistance() << endl << "Cities: ";
+    for (City &c : tour.cities) {
+        os << c;
+    }
+    return os;
+}
--- Tour.hpp
+++ Tour.hpp
@@ -5,9 +5,36 @@
 #ifndef LAB1TEMPLATE_TOUR_HPP
 #define LAB1TEMPLATE_TOUR_HPP
 
+#include "City.hpp"
+#include <vector>
+#include<ostream>
+
+using namespace std;
 
 class Tour {
+private:
+    static constexpr double MUTATION = 0.75;
+    vector<City> cities;
+    double fitness = 0;
 
+    void randomizeOrder();
+public:
+    Tour() = default;
+    explicit Tour(vector<City> &vec): cities(vec) {
+        randomizeOrder();
+        checkFitness();
+    }
+    Tour(const Tour &other);
+    void checkFitness();
+    void mutate();
+    void addCity(City &city);
+    double getFitness();
+    double getDistance();
+    bool hasCity(City &city);
+    vector<City>& getCities();
+    Tour & operator = (Tour other);
+    friend ostream &operator << (ostream &os, Tour &tour);
+    friend void swapTour(Tour &first, Tour &second);
 };
 
 
