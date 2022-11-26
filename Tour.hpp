b48c44c8b7b357c987921d0cd5e6b6335fe95a2f
Ray Zhang <rzhang104@my.bcit.ca>
2022-11-25 20:09:55 -0800

    Added some javadocs to Tour header file

--- Tour.hpp
+++ Tour.hpp
@@ -13,17 +13,39 @@ using namespace std;
 
 class Tour {
 private:
+    /**
+     * The rate of mutation; a constant.
+     */
     static constexpr double MUTATION = 0.75;
+    /**
+     * A vector of city objects.
+     */
     vector<City> cities;
+    /**
+     * The fitness of this tour.
+     */
     double fitness = 0;
-
+    /**
+     * Randomize the order of the cities in this tour.
+     */
     void randomizeOrder();
 public:
+    /**
+     * Default constructor for Tour class.
+     */
     Tour() = default;
+    /**
+     * Explicit Tour constructor initializing the cities variable to a vector of cities.
+     * @param vec a vector of cities
+     */
     explicit Tour(vector<City> &vec): cities(vec) {
         randomizeOrder();
         checkFitness();
     }
+    /**
+     * Copy constructor for Tour class.
+     * @param other a Tour class to be copied
+     */
     Tour(const Tour &other);
     void checkFitness();
     void mutate();
