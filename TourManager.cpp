cc227dcbe4a251c0aa1248117da8a201c79dee27
Ray Zhang <rzhang104@my.bcit.ca>
2022-11-25 16:30:10 -0800

    Added function to TourManager class and tested functionality.

--- TourManager.cpp
+++ TourManager.cpp
@@ -3,3 +3,120 @@
 //
 
 #include "TourManager.hpp"
+#include <iostream>
+#include <set>
+#include <random>
+
+int TourManager::findElite(vector<Tour> vec) {
+    double minFitness;
+    int eliteIndex = 0;
+    Tour elite;
+    elite = vec[0];
+    minFitness = elite.getFitness();
+
+    for (int i = 0; i < (int) vec.size(); i++) {
+        double fitness = vec[i].getFitness();
+        if (fitness < minFitness) {
+            elite = vec[i];
+            minFitness = fitness;
+            eliteIndex = i;
+        }
+    }
+    return eliteIndex;
+}
+
+void TourManager::promote() {
+    int elite = findElite(tours);
+    std::swap(tours[0], tours[elite]);
+}
+
+int TourManager::randInt(int lower, int upper) {
+    if (upper <= lower) {
+        return lower;
+    }
+    random_device random;
+    mt19937 generator(random());
+    uniform_int_distribution<> distribution(lower, upper);
+
+    return distribution(generator);
+}
+
+vector<Tour> TourManager::selectParents() {
+    int poolSize = ceil(((double) tours.size()) * 0.2);
+    vector<Tour> parentPool;
+    for (int i = 0; i < poolSize; i++) {
+        int index = randInt(1, (int) tours.size() - 1);
+        parentPool.push_back(tours[index]);
+    }
+    return parentPool;
+}
+
+Tour TourManager::createChild(Tour &father, Tour &mother) {
+    int ceiling = (int) father.getCities().size();
+    int index = randInt(0, ceiling - 1);
+    Tour child;
+    vector<City> motherland = mother.getCities();
+    vector<City> fatherland = father.getCities();
+
+    for (int i = 0 ; i <= index; i++) {
+        child.addCity(fatherland.at(i));
+    }
+
+    for (City & i : motherland) {
+        if (!child.hasCity(i)) {
+            child.addCity(i);
+        }
+    }
+
+    child.getFitness();
+    return child;
+}
+
+void TourManager::crossover() {
+    vector<Tour> children;
+    children.push_back(tours[0]); // Keep the elite
+
+    while (children.size() < tours.size()) {
+        // create a pool of potential fathers from the population.
+        vector<Tour> fathers = selectParents();
+        // select the fittest father.
+        int eliteFather = findElite(fathers);
+        Tour father = fathers.at(eliteFather);
+
+        // create a pool of mothers from the population.
+        vector<Tour> mothers = selectParents();
+        // select the fittest mother.
+        int eliteMother = findElite(mothers);
+        Tour mother = mothers.at(eliteMother);
+
+        // create a child and add it to the children vector
+        Tour child = createChild(father, mother);
+        children.push_back(child);
+    }
+    // set tours to this new vector of newly created Tours, keeping the previous elite
+    tours = children;
+}
+
+void TourManager::mutateTours() {
+    for (int i = 1; i < (int) tours.size(); i++) {
+        tours.at(i).mutate();
+    }
+}
+
+Tour TourManager::getElite() {
+    return tours.at(0);
+}
+
+void TourManager::improve() {
+    promote();
+    crossover();
+    mutateTours();
+}
+
+ostream &operator<<(ostream &os, TourManager &tourManager) {
+    for (Tour &t : tourManager.getTours){
+        os << t << "TourManager overloading <<" << endl;
+    }
+    return os;
+}
+
--- TourManager.hpp
+++ TourManager.hpp
@@ -5,9 +5,28 @@
 #ifndef LAB1TEMPLATE_TOURMANAGER_HPP
 #define LAB1TEMPLATE_TOURMANAGER_HPP
 
+#include "Tour.hpp"
+#include <vector>
+
+using namespace std;
 
 class TourManager {
-
+private:
+    vector<Tour> tours;
+    static int findElite(vector<Tour> vec);
+    void promote();
+    static int randInt(int lower, int upper);
+    vector<Tour> selectParents();
+    Tour createChild(Tour &father, Tour &mother);
+    void crossover();
+    void mutateTours();
+public:
+    TourManager() = default;
+    explicit TourManager(vector<Tour> &t) : tours(t) {}
+    Tour getElite();
+    vector<Tour> getTours;
+    void improve();
+    friend ostream & operator << (ostream &os, TourManager &tourManager);
 };
 
 
